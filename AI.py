d05d03f1b450c1a00c9abd116985a176dc3beef8
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-09-10 13:04:56 -0400

    Initial classes have been created

---
+++ AI.py
@@ -0,0 +1,6 @@
+from Player import Player
+
+
+class AI(Player):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ Game.py
@@ -0,0 +1,24 @@
+class Game:
+    def __init__(self) -> None:
+        pass
+
+    # Display rules to user (best of 3, what beats what, etc.)
+    # Human v Human or Human v AI
+    # System that checks input against each other
+    # Determine who won
+    # Continue until one wins two matches
+
+
+    
+    
+    def run_game(self):
+        pass
+
+    def display_rules(self):
+        pass
+
+    def match_sequence(self):
+        pass
+
+    def check_who_won(self):
+        pass
